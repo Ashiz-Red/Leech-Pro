dde2f827e77006a2e7f81c07ce751fbbdc077c9f
Ashiz-Red <55756535+Ashiz-Red@users.noreply.github.com>
2021-05-13 22:05:10 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,31 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+
+ TG_BOT_TOKEN= "1710514865:AAGaBcYHJyDb1COYVvZipy7kNKcb1hT65iE"
+
+ APP_ID = "5924779"
+
+ API_HASH = "619fcd44335b53c5f809df6e4695d4d2"
+
+ AUTH_CHANNEL = [-1001353157105, 1615125517]
+
+ INDEX_LINK = ""
+
+ GLEECH_COMMAND = "gleech@Safiyah_Sohail_bot"
+
+ YTDL_COMMAND = "ytdl@Safiyah_Sohail_bot"
+
+ TELEGRAM_LEECH_COMMAND_G = "tleech@Safiyah_Sohail_bot"
+
+ CLONE_COMMAND_G = "gclone@Safiyah_Sohail_bot"
+
+ PYTDL_COMMAND_G = "pytdl@Safiyah_Sohail_bot"
+
+ DESTINATION_FOLDER = "
+
+ LEECH_COMMAND = "leech@Safiyah_Sohail_bot"
+
+ CANCEL_COMMAND = "cancel@Safiyah_Sohail_bot"
+
+ RCLONE_CONFIG ="""""
